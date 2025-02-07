bf9250362a2723efb9c408e3220c99b203d35201
Eben645 <eben23713@gmail.com>
2025-02-07 21:26:56 +0000

    port

--- server.js
+++ server.js
@@ -153,6 +153,7 @@ app.delete("/delete-student", async (req, res) => {
 
 
 // Start Server
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(5000, '0.0.0.0', () => {
+    console.log("Server running on http://0.0.0.0:5000");
 });
+
